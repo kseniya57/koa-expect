a5bde8d3f2a6ac94292c701006a5b1440d43c5e9
Yanislav Igonin <jan.oasis@gmail.com>
2019-02-05 16:12:13 +0300

    Add outputSchema validation

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const expect = require('./src/expect');
 
-module.exports = expectation => async (ctx, next) => {
-  expect(ctx.params || (ctx.method === 'GET' ? ctx.query : ctx.request.body), expectation);
+module.exports = (inputSchema, outputSchema) => async (ctx, next) => {
+  if (inputSchema) {
+    expect(
+      ctx.params || (ctx.method === 'GET' ? ctx.query : ctx.request.body),
+      inputSchema,
+    );
+  }
+
   await next();
+
+  if (outputSchema) {
+    expect(ctx.body, inputSchema);
+  }
 };
